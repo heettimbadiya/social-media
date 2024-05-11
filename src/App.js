82253bc8d613a2f83f6127c33f9f68c8f71999f7
heettimbadiya <heettimbadiya650@gmail.com>
2024-05-11 09:15:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Box, useTheme } from '@mui/material'
 import React from 'react'
-import SignUp from './components/ragistration/SignUp'
 
 const App = () => {
-  const theme = useTheme()
   return (
-    <Box>
-      <SignUp />
-    </Box>
+    <div>
+      cfvg
+    </div>
   )
 }
 
